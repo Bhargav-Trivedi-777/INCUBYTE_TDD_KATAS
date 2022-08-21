3d9a37dcfd692a9f25c0883d4dab582b81d3ca18
Bhargav999 <bhargavtrivedi999@gmail.com>
2022-08-21 19:49:12 +0530

    Add test and code for 1 input value
    This test case is added for add method of StringCalculator clss which will return the sum of given value

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,3 +1,5 @@
+import java.util.StringTokenizer;
+
 public class StringCalculator {
     public static int add(String inputValue) 
     {
@@ -5,6 +7,12 @@ public class StringCalculator {
         {
             return 0;
         } 
+        
+        StringTokenizer st = new StringTokenizer(inputValue,",");
+        if(st.countTokens() == 1)
+        {
+            return Integer.parseInt(inputValue);
+        }
         return -1;
     }
 }
--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -10,4 +10,13 @@ public class StringCalculatorTest {
         int actualValue = StringCalculator.add(inputValue);
         assertEquals("it should return 0 if the String is empty",expectedValue, actualValue);
     }
+
+    @Test
+    public void hasOneValue()
+    {
+        String inputValue = "12";
+        int expectedValue = 12;
+        int actualValue = StringCalculator.add(inputValue);
+        assertEquals("it shoud return 1 if one argument is passed",expectedValue, actualValue);
+    }
 }
