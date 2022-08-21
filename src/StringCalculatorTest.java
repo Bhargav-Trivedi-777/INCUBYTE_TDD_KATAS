e7dc47cc00f7a53b270bddb44c10eb70b2e27432
Bhargav999 <bhargavtrivedi999@gmail.com>
2022-08-21 18:27:03 +0530

    Add Test and code for empty input value
    that check it should  return 0 if entered string is empty

--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -1,3 +1,13 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 public class StringCalculatorTest {
-    
+    @Test
+    public void isEmpty()
+    {
+        String inputValue ="";
+        int expectedValue = 0;
+        int actualValue = StringCalculator.add(inputValue);
+        assertEquals("it should return 0 if the String is empty",expectedValue, actualValue);
+    }
 }
